906463a99f4fed05cb23cb1d557b734a7a940492
Mohd Sohaib <144609660+Sohaib0786@users.noreply.github.com>
2026-01-08 23:48:20 +0530

    Change API URL to production endpoint

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/tasks";
+const API = "https://task-tracker-backend-u7i0.onrender.com/api/tasks";
 
 export const fetchTasks = () => axios.get(API);
 export const createTask = (data) => axios.post(API, data);
