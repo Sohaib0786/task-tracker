07996d4afc1c9c47905756e11ff056a143507e93
Mohd Sohaib <shoaibqu7714@gmail.com>
2026-01-08 23:05:49 +0530

    Initial commit - MERN Task Tracker3

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 
 connectDB();
-//shoaibqu7714_db_user
-//D0yBEDw05nSTz3ul
-//mongodb+srv://shoaibqu7714_db_user:D0yBEDw05nSTz3ul@cluster0.o7ztiwa.mongodb.net/
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
